540c8a697aecbdbf9d812d58ee7e9af974585953
Kseniia215 <ksushapovarnithina215@gmail.com>
2025-06-05 00:17:40 +0300

    Первый коммит

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+export function Footer() {
+    return(
+        <footer class="page-footer blue lighten-2">
+          <div class="footer-copyright">
+            <div class="container">
+                © {new Date().getFullYear()} Copyright Text
+                <a class="grey-text text-lighten-4 right" href="#!">
+                    Repository
+                </a>
+                <p>Выполнила: Поварницына Ксения ТКБО-01-23</p>
+            </div>
+          </div>
+        </footer>
+    )
+}
